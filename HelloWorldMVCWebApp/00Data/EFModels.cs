53cd09a3bec90971542068c11db2e60395a9b7c8
AshishLankala <48539915+AshishLankala@users.noreply.github.com>
2020-08-05 12:47:27 +0930

    Week 2 Task

---
+++ HelloWorldMVCWebApp/00Data/EFModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVCWebApp._00Data
+{
+    public class Book
+    {
+
+        public int BookId { get; set; }
+        [StringLength(50)]
+        [Required]
+
+        public string Title { get; set; }
+
+        public string PublisherTitle { get; set; }
+
+        public string Author { get; set; }
+        //public Author Author { get; set; }
+    }   
+}
+/* 
+ dotnet tool install --global dotnet-ef 
+
+dotnet ef migrations add InitialCreate   -- creates script 
+
+ 
+
+dotnet ef database update  -- creates db and runs the migration 
+
+ 
+
+dotnet ef migrations remove 
+
+dotnet ef database drop */
